8f22bf2e601668d9ec13d9fa7f8a606daf92f263
Lara-Codes <lara.palombi@gmail.com>
2023-10-05 15:04:23 -0400

    got rid of boilerplate

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class = "title">Home</h1>
+    <h2 class="subtitle">
+      Welcome to ur app
+    </h2>
   </main>
 </template>
