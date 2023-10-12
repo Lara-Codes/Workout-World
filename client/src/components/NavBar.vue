aeeac27e450f50e94a62e40dcab8e18a8d948362
Lara-Codes <lara.palombi@gmail.com>
2023-10-12 14:07:57 -0400

    commit

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,70 +1,128 @@
 <script setup lang="ts">
-    import {ref} from 'vue';
-    import { RouterLink } from 'vue-router';
-    const isActive = ref(false)
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router';
+
+let dropdown = false; 
+let isActive = ref(dropdown)
+
+function drop(){
+  dropdown = !dropdown; 
+}
+
 </script>
 
 <template>
-<nav class="navbar" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a class="navbar-item">
-      <img src="@/assets/logo.svg" width="28" height="28">
-    </a>
-
-    <a role="button" class="navbar-burger" :class="{'is-active': isActive}" @click="isActive = !isActive" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
-
-  <div id="navbarBasicExample" class="navbar-menu" :class="{'is-active': isActive}">
-    <div class="navbar-start">
-        <RouterLink class = "navbar-item" to ="/">Home</RouterLink>
-        <RouterLink class = "navbar-item" to="about">About</RouterLink>
-
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">
-          More
+  <!-- NAVBAR  -->
+  <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+    <div class="navbar-item"></div>
+    <div class="navbar-brand">
+      <a class="navbar-item">
+        <img src="@/assets/logo.svg" width="28" height="28">
+      </a>
+    </div>
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item">
+          <span class="icon">
+            <i class="fas fa-running"></i>
+          </span>
+          My Activity
+        </a>
+        <a class="navbar-item">
+          <span class="icon">
+            <i class="fas fa-chart-line"></i>
+          </span>
+          Statistics
+        </a>
+        <a class="navbar-item">
+          <span class="icon">
+            <i class="fas fa-users"></i>
+          </span>
+          Friends Activity
+        </a>
+        <a class="navbar-item">
+          <span class="icon">
+            <i class="fas fa-search"></i>
+          </span>
+          People Search
         </a>
 
-        <div class="navbar-dropdown">
-          <a class="navbar-item">
-            About
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">
+            Admin
           </a>
-          <a class="navbar-item">
-            Jobs
-          </a>
-          <a class="navbar-item">
-            Contact
-          </a>
-          <hr class="navbar-divider">
-          <a class="navbar-item">
-            Report an issue
-          </a>
+
+          <div class="navbar-dropdown">
+            <a class="navbar-item">
+              Users
+            </a>
+          </div>
+
+        </div>
+      </div>
+
+      <div class="navbar-end">
+        
+        <a class="navbar-item">
+          <strong>Sign up</strong>
+        </a>
+
+        <div class="buttons">
+          <div class="dropdown is-active">
+            <div class="dropdown-trigger">
+              <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+                <span>Login</span>
+                <span class="icon is-small">
+                  <i class="fas fa-angle-down" aria-hidden="true"></i>
+                </span>
+              </button>
+            </div>
+            <div class="dropdown-menu" id="dropdown-menu" role="menu">
+              <div class="dropdown-content">
+                <a href="#" class="dropdown-item">
+                  Moshe 
+                </a>
+                <a class="dropdown-item">
+                  Kamala Harris 
+                </a>
+                <a href="#" class="dropdown-item">
+                  John Smith 
+                </a>
+                <hr class="dropdown-divider">
+                <a href="#" class="dropdown-item">
+                  Other Account
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <button class="button">
+            <span class="icon">
+              <i class="fab fa-twitter"></i>
+            </span>
+            <a href="https://twitter.com/i/flow/login?redirect_after_login=%2Fintent%2Ftweet%3Ftext%3DBulma%3A%2520a%2520modern%2520CSS%2520framework%2520based%2520on%2520Flexbox%26hashtags%3Dbulmaio%26url%3Dhttps%3A%2F%2Fbulma.io%26via%3Djgthms"
+              target="_blank" style="color:inherit"><span>Tweet</span></a>
+          </button>
+          <div class="navbar-item"></div>
         </div>
       </div>
     </div>
 
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="buttons">
-          <a class="button is-primary">
-            <strong>Sign up</strong>
-          </a>
-          <a class="button is-light">
-            Log in
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-</nav>
+  </nav>
 </template>
 
 <style scoped>
-    .router-link-active{
-        font-weight: bold;
-        border-bottom: 2px solid #00b1d2;
-    }
+.icon {
+  padding-right: 10px;
+}
+
+.button{
+  margin:15px; 
+}
+
+.router-link-active {
+  font-weight: bold;
+  border-bottom: 2px solid #00b1d2;
+}
+
 </style>
